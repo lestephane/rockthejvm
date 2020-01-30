f458e1d198423d5e2badc7a88573de9bd45decb3
Le Stephane <me@stephane.today>
2020-01-30 19:41:55 +0200

    simplifications based on the udemy answers to exercises
    - simpler foreach
    - much simpler zipWith
    - much simpler fold
    - slightly less verbose toCurry and fromCurry

--- udemy-scala-beginners/src/lectures/part3fp/HOFsAndCurries.scala
+++ udemy-scala-beginners/src/lectures/part3fp/HOFsAndCurries.scala
@@ -4,22 +4,22 @@ import lectures.part3fp.HOFsAndCurries.{andThen, compose, fromCurry, toCurry}
 
 object HOFsAndCurries extends App {
   // 2. toCurry(f: (Int, Int)) => (Int => Int => Int)
-  def toCurry(f: (Int, Int) => Int): Int => Int => Int =
+  def toCurry[A,B,C](f: (A, B) => C): A => B => C =
     x => y => f(x, y)
 
   // 2. fromCurry(f: (Int => Int => Int)) => (Int, Int) => Int
-  def fromCurry(f: (Int => Int => Int)): (Int, Int) => Int =
+  def fromCurry[A, B, C](f: A => B => C): (A, B) => C =
     (x, y) => f(x)(y)
 
   // 3. compose(f,g) => x => f(g(x))
-  def compose(f: Int => Int, g: Int => Int): (Int) => Int = x => f(g(x))
+  def compose[A,B,C](f: B => C, g: A => B): A => C = x => f(g(x))
 
   // 3. andThen(f,g) => x => g(f(x))
-  def andThen(f: Int => Int, g: Int => Int): (Int) => Int = x => g(f(x))
+  def andThen[A,B,C](f: A => B, g: B => C): A => C = x => g(f(x))
 }
 
 object CurryTest extends App {
-  val curryMult = toCurry(_ * _)
+  val curryMult = toCurry((x: Int, y: Int) => x * y)
   assert(curryMult(2)(3) == 6)
 
   val mult = fromCurry(curryMult)
