23dc06ae4d52302526f607d3d871025286ac798b
Le Stephane <me@stephane.today>
2020-02-06 06:54:49 +0200

    commit changes before working on filesystem assignment

--- udemy-scala-beginners/src/exercises/AllTests.scala
+++ udemy-scala-beginners/src/exercises/AllTests.scala
@@ -1,6 +1,6 @@
 package exercises
 
-import lectures.part3fp.{CombineTest, CurryTest}
+import lectures.part3fp.{CombineTest, CurryTest, HOFsAndCurries, MapFlatmapFilterFor, TuplesAndMaps, WhatsAFunction}
 
 object AllTests extends App {
   private val noargs: Array[String] = Array[String]()
@@ -9,11 +9,13 @@ object AllTests extends App {
   FoldTest.main(noargs)
   ForeachTest.main(noargs)
   ForTest.main(noargs)
+  HOFsAndCurries.main(noargs)
   MaybeTest.main(noargs)
   MergeTest.main(noargs)
   MyListTest.main(noargs)
   SocialNetworkTest.main(noargs)
   SortTest.main(noargs)
   SplitTest.main(noargs)
+  WhatsAFunction.main(noargs)
   ZipWithTest.main(noargs)
 }
--- udemy-scala-beginners/src/lectures/part3fp/WhatsAFunction.scala
+++ udemy-scala-beginners/src/lectures/part3fp/WhatsAFunction.scala
@@ -5,8 +5,6 @@ object WhatsAFunction extends App {
     override def apply(v1: String, v2: String): String = v1 + v2
   }
 
-  println(concat("1", "2"))
-
   val makeMultiplier = new (Int => Int => Int) {
     override def apply(x: Int): Int => Int = (n: Int) => x * n
   }
